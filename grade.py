2ddcc671fb337bbc4b3ed1aec19eedf71885a9ac
emily blick <eblick@umd.edu>
2018-11-21 23:24:26 -0500

    added code to convert ipynb files

--- grade.py
+++ grade.py
@@ -3,8 +3,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import difflib
 from interruptingcow import timeout
+import nbformat
+from nbconvert import PythonExporter
+import timeit
 
-directory = sys.argv[1]
+
+
+#Your statements here
+
+
+
+print('Time: ', stop - start)
+
+# function to convert a jupyter notebook to a python file
+def convertNotebook(notebookPath, modulePath):
+    
+    with open(notebookPath) as fh:
+        nb = nbformat.reads(fh.read(), nbformat.NO_CONVERT)
+            
+    exporter = PythonExporter()
+    source, meta = exporter.from_notebook_node(nb)
+
+    with open(modulePath, 'wb') as fh:
+        fh.writelines([source.encode('utf-8')])
+
+# read in submission directory
+directory = sys.argv[1] if sys.argv[1][-1:] == '/' else ''.join([sys.argv[1],'/'])
 
 comments = []
 
@@ -14,27 +38,35 @@ filenames = sorted(os.listdir(directory))
 for filename in filenames:
     err = 'none'
     print(filename)
+    # if file is a jupyter notebook, convert it to a python file
+    if filename.endswith(".ipynb"):
+        convertNotebook(''.join([directory,filename]), ''.join([directory,filename[:-6],'.py']))
+        filename = ''.join([filename[:-6],'.py'])
+        print(filename)
+    # if file is a python file, try to run it
     if filename.endswith(".py"):
+        start = timeit.default_timer()
         try:
-            with open(''.join([directory,filename])) as file:
-                with timeout(120, exception=RuntimeError):
-                    exec(file.read())
-                    plt.show()
-                    plt.close()                  
+            with open(''.join([directory,filename])) as file, timeout(120, exception=RuntimeError):
+                exec(file.read())
+                # show any plots created in the file using matplotlib
+                plt.show()
+                plt.close()
         except Exception as e:
             print('There was an error: ' + str(e))
             err = str(e)
             pass
-    comments.append((filename, str(err), 'True' if filename.endswith(".py") else 'False', input("Comments: ")))
+        stop = timeit.default_timer()
+    comments.append((filename, str(err), stop-start, 'True' if filename.endswith(".py") else 'False', input("Comments: ")))
 
 # save grading csv
-df = pd.DataFrame(comments, columns = ['Filename','Error','.py file','Comments'])
+df = pd.DataFrame(comments, columns = ['Filename','Error','Runtime','.py file','Comments'])
 df.to_csv(''.join([directory,'grades.csv']))
 
 code = []
 pyfiles = []
 
-# diff loop
+# diff loop to check file similarity
 for filename in filenames:
     if filename.endswith(".py"):
         pyfiles.append(filename[:filename.find('_')])
